2ce882ea915c42e579a7c2770cb5be697f8a1f6a
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-04-06 16:14:11 +1100

    Ported MOCConstants
    to 1.16.5

---
+++ src/main/java/com/drzhark/mocreatures/MoCConstants.java
@@ -0,0 +1,10 @@
+package com.drzhark.mocreatures;
+
+public class MoCConstants {
+
+    public static final String MOD_ID = "mocreatures";
+    public static final String MOD_PREFIX = MOD_ID + ":";
+    public static final String MOD_NAME = "DrZhark's Mo'Creatures";
+    public static final String MOD_VERSION = "12.1.0";
+    public static final String MOD_ACCEPTED_VERSIONS = "[12.1.0]";
+}
